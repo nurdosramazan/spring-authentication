36b06a2560972f14a4fdbf15b88d92b06937af71
nurdosramazan <nurdos.ramazan@gmail.com>
2025-03-09 16:59:03 +0500

    initial commit

---
+++ src/test/java/kz/nurdos/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.nurdos.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
